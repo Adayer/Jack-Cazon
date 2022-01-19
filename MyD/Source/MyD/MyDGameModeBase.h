f2f551f7a68eb42555ae7d601fa24ea318d7cd6a
Adayer <aday3r@gmail.com>
2022-01-19 21:36:32 +0100

    Init Project

---
+++ MyD/Source/MyD/MyDGameModeBase.h
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYD_API AMyDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
